360a9ea99d0f7e98cf97180d233a34a442ebe10d
hosseintalebi <htalebi@hotmail.com>
2017-11-18 15:20:13 -0700

    Sky component

---
+++ src/components/sky/Sky.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import _ from 'lodash'
+import './styles.css'
+
+export default class Sky extends Component {
+  render () {
+    return (
+      <div className="sky">
+        <div className="cloud1">
+          <div className="c1">
+          </div>
+          <div className="c2">
+          </div>
+          <div className="c3">
+          </div>
+          <div className="c4">
+          </div>
+        </div>
+        <div className="cloud2">
+          <div className="c1">
+          </div>
+          <div className="c2">
+          </div>
+          <div className="c3">
+          </div>
+          <div className="c4">
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
