7e1d698479f97e02fa06b7addefebdce5063eff4
hosseintalebi <htalebi@hotmail.com>
2017-11-25 14:52:38 -0700

    initial screen

---
+++ src/components/initialScreen/InitialScreen.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import './styles.css'
+
+export default class Ground extends Component {
+  render () {
+    const { openPresent } = this.props
+    return (
+      <div className="initialScreen">
+        <div className="cake">
+          <div className="velas">
+            <div className="fuego"></div>
+            <div className="fuego"></div>
+            <div className="fuego"></div>
+            <div className="fuego"></div>
+            <div className="fuego"></div>
+          </div>
+          <div className="cobertura"></div>
+          <div className="bizcocho"></div>
+          <h1>happy birthday!</h1>
+          <p>Allie</p>
+        </div>
+      </div>
+    )
+  }
+}
